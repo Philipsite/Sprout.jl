5c4cb3f692b3f9006dc82a8eda8e0792afe21201
philip <philip.hartmeier@unil.ch>
2025-09-15 14:06:54 +0200

    Make connection() work with StridedArrays - Allows to leverages GPU computations (CUBLAS)

--- src/model.jl
+++ src/model.jl
@@ -1,11 +1,15 @@
 """
 connection fucntion used within compund models Parallel layer to reduce ŷ_classifier with ŷ_regressor.
 """
-function connection(y_clas::Matrix, y_reg::Matrix)
-    y_phase_frac = y_reg[1:22, :] .* y_clas
+function connection(y_clas::T, y_reg::T) where {T <: Union{Matrix, CuArray}}
+    # using @view to get a StridedArray and avoid Scalar indexing
+    y_phase_frac = @view(y_reg[1:22, :]) .* y_clas
 
-    y_ss_vecs = [y_reg[22+(6*(i-1)+1):22+(6*i), :] .* repeat(y_clas[7+i:7+i, :], inner=(6,1)) for i = 1:15]
-    y_ss_comp = vcat(y_ss_vecs...)
+    reg_indices  = [22 + (6*(i-1)+1):22 + (6*i) for i in 1:15]
+    clas_indices = [7+i:7+i for i in 1:15]
+
+    y_ss_comp = mapreduce((reg_indices, clas_indices) -> @view(y_reg[reg_indices, :]) .* repeat(@view(y_clas[clas_indices, :]), 6, 1),
+                          vcat, reg_indices, clas_indices)
 
     return vcat(y_phase_frac, y_ss_comp)
 end
