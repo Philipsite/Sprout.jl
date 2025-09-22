ab3be704bd3b4e4cbdd7dbd8cbf652f0e369c624
Philipsite <philip.hartmeier@hotmail.com>
2025-09-22 11:40:08 +0200

    Update generate_dataset() - Adapt molar basis for bulk/SS composition - Commented out densities (phase-wise) and added bulk density extraction - Updated tests accordingly

--- src/gen_data.jl
+++ src/gen_data.jl
@@ -33,13 +33,13 @@ function generate_dataset(n::Int, filename_base::String;
     # use bulk_S as bulk composition, test with "molar conservence" showed less deviation.
     # Is this because the mass residual in MAGEMin is not included into bulk_S? > Check with nico.
     # //ANCHOR - This only works for SB21 which has no liquid phases, not given that this goes trough with other databases
-    bulks = reduce(hcat, [out_i.bulk_wt for out_i in out])
+    bulks = reduce(hcat, [out_i.bulk_S for out_i in out])
 
     oxides_in_out = Matrix{String}(undef, 6, n)
-    ph_vol  = zeros(22, n)
-    ph_ρ    = zeros(22, n)
+    ph_mode  = zeros(22, n)
+    # ph_ρ    = zeros(22, n)
     ss_comp = zeros(90, n)
-    modulus = zeros(2, n)
+    phys_prop = zeros(3, n)
 
     @threads for i in ProgressBar(eachindex(out))
         oxides_in_out[:, i] .= out[i].oxides
@@ -48,29 +48,29 @@ function generate_dataset(n::Int, filename_base::String;
         indices_phaselist = [findfirst(.==(p), phase_list) for p in ph_i]
         is_ss = Bool.(out[i].ph_type)
 
-        # add bulk and shear modulus
-        modulus_i = vcat(out[i].bulkMod, out[i].shearMod)
-        modulus[:, i] .= modulus_i
+        # add density, bulk- and shear-modulus
+        phys_prop_i = vcat(out[i].rho, out[i].bulkMod, out[i].shearMod)
+        phys_prop[:, i] .= phys_prop_i
 
-        # add ph_vol
-        ph_vol_i = out[i].ph_frac_vol
-        ph_vol[indices_phaselist, i] .= ph_vol_i
+        # add ph_mode
+        ph_mode_i = out[i].ph_frac
+        ph_mode[indices_phaselist, i] .= ph_mode_i
 
-        # add ph_ρ
-        ph_ρ_i = []
-        SS_idx = 0
-        PP_idx = 0
-        for j = eachindex(ph_i)
-            if is_ss[j]
-                SS_idx += 1
-                ρ = out[i].SS_vec[SS_idx].rho
-            else
-                PP_idx += 1
-                ρ = out[i].PP_vec[PP_idx].rho
-            end
-            push!(ph_ρ_i, ρ)
-        end
-        ph_ρ[indices_phaselist, i] .= ph_ρ_i
+        # # add ph_ρ
+        # ph_ρ_i = []
+        # SS_idx = 0
+        # PP_idx = 0
+        # for j = eachindex(ph_i)
+        #     if is_ss[j]
+        #         SS_idx += 1
+        #         ρ = out[i].SS_vec[SS_idx].rho
+        #     else
+        #         PP_idx += 1
+        #         ρ = out[i].PP_vec[PP_idx].rho
+        #     end
+        #     push!(ph_ρ_i, ρ)
+        # end
+        # ph_ρ[indices_phaselist, i] .= ph_ρ_i
 
         # add ph_comp (only ss phases considered)
         # update indices_phaselist to only consider the solid solutions (ss)
@@ -80,7 +80,7 @@ function generate_dataset(n::Int, filename_base::String;
         # adjust indices to ranges, e.g. idx 1 > 1:6, idx 5 > 25:30, etc.
         indices_phaselist = vcat([vcat((idx-1)*6+1:(idx-1)*6+6) for idx in indices_phaselist]...)
 
-        ph_comp_i = reduce(vcat, [ss.Comp_wt for ss in out[i].SS_vec])
+        ph_comp_i = reduce(vcat, [ss.Comp for ss in out[i].SS_vec])
         ss_comp[indices_phaselist,i] .= ph_comp_i
     end
 
@@ -94,10 +94,16 @@ function generate_dataset(n::Int, filename_base::String;
 
     # write a CSV
     x_names = ["p_kbar", "t_c", oxides_in_out...]
-    y_names = [(phase_list .* "_vol")..., (phase_list .* "_rho")..., vcat([repeat([ss], 6) .* ("_" .* oxides_in_out)  for ss in SS]...)..., "bulk_modulus", "shear_modulus"]
+    y_names = [(phase_list .* "_vol")...,
+               # (phase_list .* "_rho")...,
+               vcat([repeat([ss], 6) .* ("_" .* oxides_in_out)  for ss in SS]...)...,
+               "bulk density", "bulk_modulus", "shear_modulus"]
 
     x_data = vcat(pressure_kbar', temperature_C', bulks)
-    y_data = vcat(ph_vol, ph_ρ, ss_comp, modulus)
+    y_data = vcat(ph_mode,
+                  # ph_ρ,
+                  ss_comp,
+                  phys_prop)
 
     x_data = DataFrame(x_data', Symbol.(x_names))
     y_data = DataFrame(y_data', Symbol.(y_names))
