cf9b554c9e1f3d5437d453a0f074249b25815b53
philip <philip.hartmeier@unil.ch>
2025-09-23 18:53:30 +0200

    Add preprocess_for_regressor() and corresponding tests

--- src/preprocessing.jl
+++ src/preprocessing.jl
@@ -21,6 +21,26 @@ function preprocess_for_classifier(x_data::DataFrame, y_data::DataFrame)
     return x::Matrix, y::Union{Matrix, BitMatrix}
 end
 
-function preprocess_for_regressor(x_data::DataFrame, y_data::DataFrame)
 
+"""
+Takes DataFrame of Training/Validation/Test data, returns a Matrix with each vector (vector of features) being an independent datapoint.
+Filters to only extract phases that are predicted as part of the stable assemblage at least once in the dataset.
+For the SS composition only variable components are exracted, e.g. no Si in Olivine as this is constant.
+"""
+function preprocess_for_regressor(x_data::DataFrame, y_data::DataFrame)
+    x = Matrix(Matrix{Float32}(x_data)')
+
+    # set up indices of stable phases
+    idx_stable_phases = [i for i in 1:22 if i ∉ IDX_of_phases_never_stable]
+    # set up indices of compositional entries in SS that are variable, that belong to stable phases
+    IDX_of_SS_never_stable = [i for i in IDX_of_phases_never_stable if i > 7] .- 7
+    idx_SS_variable_and_stable = [i for i in IDX_of_variable_components_in_SS if i ∉ [k for j in IDX_of_SS_never_stable for k in (j-1)*6+1:j*6]]
+
+    # correct to start at index 23
+    idx_SS_variable_and_stable .+= 22
+
+    n_features = size(y_data, 2)
+    y = Matrix(Matrix{Float32}(y_data)')[[idx_stable_phases..., idx_SS_variable_and_stable..., n_features-2, n_features-1, n_features],:]
+
+    return x::Matrix, y::Matrix
 end
